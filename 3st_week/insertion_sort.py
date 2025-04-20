95219801ea29d909a70a0c97eef60dc78c210e3c
sangwook <bnbt3@naver.com>
2025-04-20 23:06:10 +0900

    정렬 알고리즘 구현: 버블 정렬(bubble_sort), 선택 정렬(selection_sort), 삽입 정렬(insertion_sort) 추가

---
+++ 3st_week/insertion_sort.py
@@ -0,0 +1,19 @@
+input = [4, 6, 2, 9, 1]
+
+
+def insertion_sort(array):
+    result = [array[0]]
+    for a in range(1, len(array)):
+        result.append(array[a])
+        for b in range(a, 0, -1):
+            if result[b] < result[b - 1]:
+                result[b - 1], result[b] = result[b], result[b - 1]
+
+    return result
+
+
+print(insertion_sort(input))  # [1, 2, 4, 6, 9] 가 되어야 합니다!
+
+print("정답 = [4, 5, 7, 7, 8] / 현재 풀이 값 = ",insertion_sort([5,8,4,7,7]))
+print("정답 = [-1, 3, 9, 17] / 현재 풀이 값 = ",insertion_sort([3,-1,17,9]))
+print("정답 = [-3, 32, 44, 56, 100] / 현재 풀이 값 = ",insertion_sort([100,56,-3,32,44]))
