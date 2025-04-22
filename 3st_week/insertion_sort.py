694c9fc22ae8685886a394eb9a6a5115a5003c09
sangwook <bnbt3@naver.com>
2025-04-22 22:17:19 +0900

    분할정복

--- 3st_week/insertion_sort.py
+++ 3st_week/insertion_sort.py
@@ -14,6 +14,6 @@ def insertion_sort(array):
 
 print(insertion_sort(input))  # [1, 2, 4, 6, 9] 가 되어야 합니다!
 
-print("정답 = [4, 5, 7, 7, 8] / 현재 풀이 값 = ",insertion_sort([5,8,4,7,7]))
-print("정답 = [-1, 3, 9, 17] / 현재 풀이 값 = ",insertion_sort([3,-1,17,9]))
-print("정답 = [-3, 32, 44, 56, 100] / 현재 풀이 값 = ",insertion_sort([100,56,-3,32,44]))
+print("정답 = [4, 5, 7, 7, 8] / 현재 풀이 값 = ", insertion_sort([5, 8, 4, 7, 7]))
+print("정답 = [-1, 3, 9, 17] / 현재 풀이 값 = ", insertion_sort([3, -1, 17, 9]))
+print("정답 = [-3, 32, 44, 56, 100] / 현재 풀이 값 = ", insertion_sort([100, 56, -3, 32, 44]))
